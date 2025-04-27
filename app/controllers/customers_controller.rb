5e9c5c5c08598aeb0e7bc4869c2de9982f6ba0e6
nikhilghadi <nikhilghadi665@gmail.com>
2025-04-27 18:23:42 +0530

    some comments added and basic sanitization added for data parsing

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,6 +1,6 @@
 class CustomersController < ApplicationController
   protect_from_forgery with: :exception
-  skip_before_action :verify_authenticity_token, only: [:upload]
+  skip_before_action :verify_authenticity_token, only: [:upload] if Rails.env.test?
   before_action :validate_file, only: [:upload]
 
   def index
@@ -14,7 +14,11 @@ class CustomersController < ApplicationController
     distance = distance == 0 ? INVITE_DISTANCE_KM : distance
     # giving ability to add distance to the user if user dont add the distance the consider 100 km as a default value
    
+    # Can add timeout if file is taking to much time to parse and filter
    
+    # We can also rate limit this API so someone cant overload our server.
+
+    
     file_processor = FileProcessor.new(@file)
     all_customers = file_processor.parse_customers
 
--- app/services/file_processor.rb
+++ app/services/file_processor.rb
@@ -7,10 +7,11 @@ class FileProcessor
   # parse txt file and create Customer objects
   def parse_customers(invite_distance = INVITE_DISTANCE_KM)
     customers = []
-        
+    # TODO
+    # In future we can parse the large file in background using sidekiq
     @file.each_line do |line|
-      customer_data = JSON.parse(line)
-      if customer_data.present?
+      customer_data = JSON.parse(line) rescue nil
+      if is_valid_data?(customer_data)
         user_id, name, latitude, longitude = customer_data["user_id"].to_i, customer_data["name"], customer_data["latitude"].to_f, customer_data["longitude"].to_f
         customer = Customer.new(
           latitude: latitude,
@@ -18,13 +19,13 @@ class FileProcessor
           user_id: user_id,
           name: name
         )
-        # # calcuating distance from office latitude and lingitude
-        # distance = customer.distance_from()
-        # if distance <= invite_distance
-          customers << customer
-        # end
+        customers << customer
       end
     end
     customers
+  end
+
+  def is_valid_data?(data)
+    data.present? && data["user_id"].present? && data["name"].present? && data["latitude"].present? && data["longitude"].present?
   end
 end
